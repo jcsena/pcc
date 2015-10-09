0928f43e43ab837b6437b7e42fdee3138427be08
Julio Sena <julio.657.js@gmail.com>
2015-10-09 09:36:00 -0500

    add tareas

---
+++ laboratorios/3/3/app.cpp
@@ -0,0 +1,48 @@
+/**
+Programa que pide dos numeros enteros por teclado
+y deternmina si es divisible o no divisible
+ */
+
+#include <iomanip>
+#include <iostream>
+
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"      /* Black */
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define BLUE    "\033[34m"      /* Blue */
+#define MAGENTA "\033[35m"      /* Magenta */
+#define CYAN    "\033[36m"      /* Cyan */
+#define WHITE   "\033[37m"      /* White */
+#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
+#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
+#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
+#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
+#define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
+#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
+#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  /**
+   * @entero1: entero 1 entero capturado por pantalla
+   * @entero2: entero 2 entero capturado por pantalla
+   */
+
+  int entero1, entero2;
+
+   //se reciben los valores enteros
+   cout << "Por favor digite el primer entero: ";
+   cin >> entero1;
+   cout << "Por favor digite el segundo entero: ";
+   cin >> entero2;
+
+   if(entero1%entero2 == 0){
+     cout << BOLDMAGENTA << entero1 << BOLDYELLOW <<" es divisible entre " << BOLDCYAN << entero2 <<endl;
+   }
+
+
+}
---
+++ tareas/4/1/app.cpp
@@ -0,0 +1,39 @@
+/**
+ Hacer un programa que lea 2 numeros
+ sume los numeros
+ si la suma es mayor o igual a 100  imprimir mayor o igual a 100
+ si no cumple con la regla
+
+
+ */
+
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+
+  /**
+   * @num1: Numero real ingreasado por patalla para realiza operacion suma
+   * @num2: Numero real ingreasado por patalla para realiza operacion suma
+   * @suma : Operacion suma realizada con los numeros @num1 y @num2
+   */
+  float num1, num2, suma;
+
+  //se solitan los dos numeros a ingresar por pantalla
+  cout << "Por favor escriba dos numeros: ";
+  cin >> num1 >> num2;
+
+  //se realiza la operacion de suma
+  suma = num1 + num2;
+
+  //se valida que la condicion de la suma sea mayor o igual a 100
+  if(suma >= 100)
+  cout << "La suma realizada es mayor a 100" << endl;
+  else
+  cout << "La suma de los numeros NO cumple con la condicion" << endl;
+
+
+  return 0;
+}
